ba9b34ce8826a491a2bae01ae22eb43deec580e4
quvisar <112892761+quvisar@users.noreply.github.com>
2023-12-03 21:51:51 +0200

    Add files via upload

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,12 +1,14 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.menu');
 let switche = document.querySelector('.switch');
+let body = document.querySelector('body');
 
 burger.addEventListener('click',click);
 
 function click(){
+	console.log(body);
 	burger.classList.toggle('active');
 	menu.classList.toggle('active');
 	switche.classList.toggle('active');
-
+	body.classList.toggle('active');
 }
