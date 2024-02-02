b01d3323af708566044a81495c92838f9ec7d6a0
quvisar <112892761+quvisar@users.noreply.github.com>
2024-02-02 22:42:39 +0200

    Add files via upload

--- js/sctipt.js
+++ js/sctipt.js
@@ -2,13 +2,14 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header--items');
 let lang = document.querySelector('.header--lang');
-let body = document.querySelector('html');
+let html = document.querySelector('html');
 
 burger.addEventListener("click",active);
+menu.addEventListener("click",active);
 
 function active() {
 	burger.classList.toggle('active');
 	menu.classList.toggle('active');
 	lang.classList.toggle('active');
-	body.classList.toggle('active');
+	html.classList.toggle('active');
 }
