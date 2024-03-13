e0f8bdc8d0d9f336ecd9ad951e4e4b94d62b863b
quvisar <112892761+quvisar@users.noreply.github.com>
2024-03-13 22:23:41 +0200

    Add files via upload

--- js/sctipt.js
+++ js/sctipt.js
@@ -4,9 +4,12 @@ let menu = document.querySelector('.header--items');
 let lang = document.querySelector('.header--lang');
 let html = document.querySelector('html');
 let wrap = document.querySelector('.wrapper');
+let question = document.querySelectorAll('.block7--item');
+
 
 burger.addEventListener("click",active);
 menu.addEventListener("click",active);
+
 
 function active() {
 	burger.classList.toggle('active');
@@ -14,4 +17,33 @@ function active() {
 	lang.classList.toggle('active');
 	html.classList.toggle('active');
 	wrap.classList.toggle('active');
+}
+
+for(let n of question){
+		n.addEventListener("click",active2);
+	}
+function active2(){
+	if(this.querySelector('.block7--item-text').classList.contains('active')){
+		this.querySelector('.block7--item-text').classList.remove('active')
+		this.classList.remove('active');
+	}
+	else{
+		this.classList.add('active');
+		this.querySelector('.block7--item-text').classList.add('active')
+		let answer = document.querySelectorAll('.block7--item-text');
+		for(let i of answer){
+			if(i==this.querySelector('.block7--item-text')){
+				continue;
+			}else{
+				i.classList.remove('active');
+			}
+		}
+		for(y of question){
+			if(y==this){
+				continue;
+			}else{
+				y.classList.remove('active');
+			}
+		}
+	}
 }
