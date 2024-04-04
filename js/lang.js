6d3bc01fc6489a03cce86241ead9e186322b66ce
quvisar <112892761+quvisar@users.noreply.github.com>
2024-04-04 23:17:09 +0300

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -1,6 +1,15 @@
+
 let changeLangBtn = document.querySelector('.header--lang').querySelectorAll('input');
 
 let changeLang = {
+	logo:{
+		Ru:'Инстаботы',
+		En:'instabots',
+	},
+	headerItems:{
+		Ru:'Меню',
+		En:'Menu',
+	},
 	about:{
 		Ru:'о компании',
 		En:'about company',
@@ -16,22 +25,34 @@ let changeLang = {
 	feedback:{
 		Ru:'обратная связь',
 		En:'feedback',
-	}
+	},
+	// block1
+
+	blockTitle:{
+		Ru:'Высокий уровень вовлечения представителей',
+		En:'High level of involvement of representatives',
+	},
+	submit:{
+		Ru:'Отправить',
+		En:'Send',
+	},
 }
 
 
 for(let btn of changeLangBtn){
 	btn.addEventListener("change",change);
 }
+
+
 function change() {
 	for(let y in changeLang){
-		let lng = document.querySelector('.lg-'+y);
-		for(let i of changeLangBtn){
-			if(i.checked){
-				lng.innerHTML = changeLang[y][i.id];
+		let lng = document.querySelectorAll('.lg-'+y);
+		for(let x of lng){
+			for(let i of changeLangBtn){
+				if(i.checked){
+					x.innerHTML = changeLang[y][i.id].charAt(0).toUpperCase()+changeLang[y][i.id].slice(1);
+				}
 			}
-		}		
+		}	
 	}
 }
-
-
